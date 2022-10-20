5f8f5cae60bd1b01867a413da57a5f7ce45e4a39
Deimar <deimarparn14@gmail.com>
2022-10-20 17:56:57 +0300

    Created a new css file for changing the application

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,35 +3,40 @@ var output = document.getElementById('demo');
 var views = document.getElementById('views');
 
 slider.oninput = function() {
+   
+    valPercent = (this.value / this.max)  * 100;
+    var color = `linear-gradient(to right,  hsl(174, 86%, 45%), ${valPercent}%, rgb(214,214,214) ${valPercent}%)`;
+    slider.style.background = color;
+    
     var price = new Array(5);
         price[0] = 500;
         price[1] = 100;
         price[2] = 50;
         price[3] = 10;
         price[4] = 1;
-        
+
         if (this.value == 36) {
-        output.innerHTML = "$" + this.value + "/month"
+        output.innerHTML = "$" + this.value 
         views.innerHTML = price[4] + "M pageviews"
         } 
 
         if (this.value == 24) {
-            output.innerHTML = "$" + this.value + "/month"
+            output.innerHTML = "$" + this.value
             views.innerHTML = price[0] + "k pageviews"
         }
    
         if (this.value == 16) {
-            output.innerHTML = "$" + this.value + "/month"
+            output.innerHTML = "$" + this.value 
             views.innerHTML = price[1] + "k pageviews"
         } 
 
         if (this.value == 12) {
-            output.innerHTML = "$" + this.value + "/month"
+            output.innerHTML = "$" + this.value 
             views.innerHTML = price[2] + "k pageviews"
         }
 
         if (this.value == 8) {
-            output.innerHTML = "$" + this.value + "/month"
+            output.innerHTML = "$" + this.value 
             views.innerHTML = price[3] + "k pageviews"
         }
 }
--- assets/js/switch.js
+++ assets/js/switch.js
@@ -8,63 +8,73 @@ var price = new Array(5);
             price[2] = 16;
             price[3] = 24;
             price[4] = 36;
-var priceafter = [
-Math.round(((price[0] * 12) - (price[0] * 12) * 25 /100 )).toFixed(0),
-Math.round(((price[1] * 12) - (price[1] * 12) * 25 /100 )).toFixed(0), 
-Math.round(((price[2] * 12) - (price[2] * 12) * 25 /100 )).toFixed(0),
-Math.round(((price[3] * 12) - (price[3] * 12) * 25 /100 )).toFixed(0),
-Math.round(((price[4] * 12) - (price[4] * 12) * 25 /100 )).toFixed(0),
+/*var priceafter = [
+Math.round(((price[0] * 12) - (price[0] * 12) * 25 /100 )).toFixed(2),
+Math.round(((price[1] * 12) - (price[1] * 12) * 25 /100 )).toFixed(2), 
+Math.round(((price[2] * 12) - (price[2] * 12) * 25 /100 )).toFixed(2),
+Math.round(((price[3] * 12) - (price[3] * 12) * 25 /100 )).toFixed(2),
+Math.round(((price[4] * 12) - (price[4] * 12) * 25 /100 )).toFixed(2),
 ];
+*/
 
 toggle.onclick = function() {
-
-    
-    if((toggle).checked == true){
+  if((toggle).checked == true){  
 
      slider.oninput = function() {
+      valPercent = (this.value / this.max)  * 100;
+      var color = `linear-gradient(to right,  hsl(174, 86%, 45%), ${valPercent}%, rgb(214,214,214) ${valPercent}%)`;
+      slider.style.background = color;
+      
+      var priceafter = Math.round(((this.value * 12) - (this.value * 12) * 25 /100 )).toFixed(2);
+
       if (this.value == 8) {
-       demo.innerHTML = '$' + priceafter[0] + '/year';
+       demo.innerHTML = '$' + priceafter
        views.innerHTML = "10k pageviews"
      } 
       if (this.value == 12) {
-       demo.innerHTML = '$' + priceafter[1] + '/year';
+       demo.innerHTML = '$' + priceafter  
        views.innerHTML = "50k pageviews"
      } 
       if (this.value == 16) {
-       demo.innerHTML = '$' + priceafter[2] + '/year';
+       demo.innerHTML = '$' + priceafter
        views.innerHTML = "100k pageviews"
      } 
       if (this.value == 24) {
-       demo.innerHTML = '$' + priceafter[3] + '/year';
+       demo.innerHTML = '$' + priceafter
        views.innerHTML = "500k pageviews"
      } 
       if (this.value == 36) {
-       demo.innerHTML = '$' + priceafter[4] + '/year';
+       demo.innerHTML = '$' + priceafter
        views.innerHTML = "1M pageviews"
      } 
 
 
-
+     
      else if((toggle).checked == false){
+
       slider.oninput = function() {
+        valPercent = (this.value / this.max)  * 100;
+        var color = `linear-gradient(to right,  hsl(174, 86%, 45%), ${valPercent}%, rgb(214,214,214) ${valPercent}%)`;
+        slider.style.background = color;
+        
         if (this.value == 8) {
-          demo.innerHTML = '$' + price[0] + '/month';
+          demo.innerHTML = '$' + price[0] 
           views.innerHTML = "10k pageviews"
       } 
         if (this.value == 12) {
-          demo.innerHTML = '$' + price[1] + '/month';
+          demo.innerHTML = '$' + price[1] 
           views.innerHTML = "50k pageviews"
       } 
         if (this.value == 16) {
-          demo.innerHTML = '$' + price[2] + '/month';
+          demo.innerHTML = '$' + price[2] 
           views.innerHTML = "100k pageviews"
       } 
         if (this.value == 24) {
-          demo.innerHTML = '$' + price[3] + '/month';
+          demo.innerHTML = '$' + price[3]
           views.innerHTML = "500k pageviews"
       } 
         if (this.value == 36) {
-          demo.innerHTML = '$' + price[4] + '/month';
+          demo.innerHTML = '$' + price[4] 
           views.innerHTML = "1M pageviews"
       } 
         }
@@ -72,3 +82,4 @@ toggle.onclick = function() {
     }
   }
 }  
+
